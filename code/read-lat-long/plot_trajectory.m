6fd86c30201351af3616b07a68bfa34826419697
mtkoroglu <m.tahakoroglu@gmail.com>
2021-12-23 02:06:02 +0300

    Add files via upload

--- code/read-lat-long/plot_trajectory.m
+++ code/read-lat-long/plot_trajectory.m
@@ -1,9 +1,16 @@
 clear; close; clc;
-load koordinatlar2.mat
+txtFile = true;
+matFile = ~txtFile;
+if (txtFile)
+    coordinates = single(load('gps data 11-Dec-2021 15-16-15.txt'))
+else
+    load koordinatlar2.mat
+end
+% dlmwrite('gap data.txt', coordinates, 'delimiter', '\t', 'precision', '%.7f')
 figure(2);
-plot(coordinates(:,2), coordinates(:,1), 'k.');
+plot(coordinates(:,3), coordinates(:,2), 'k.');
 grid on; set(gca, 'gridlinestyle', '--');
 xlabel('Boylam'); ylabel('Enlem');
 s = 0.0001;
-axis([min(coordinates(:,2))-s, max(coordinates(:,2))+s, ...
-    min(coordinates(:,1))-s max(coordinates(:,1))+s]);
+axis([min(coordinates(:,3))-s, max(coordinates(:,3))+s, ...
+    min(coordinates(:,2))-s max(coordinates(:,2))+s]);
