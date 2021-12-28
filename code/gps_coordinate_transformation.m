22b6d4d674385b30660e6edeb3e682ed6047b4f3
mtkoroglu <m.tahakoroglu@gmail.com>
2021-12-28 23:12:46 +0300

    Update gps_coordinate_transformation.m

--- code/gps_coordinate_transformation.m
+++ code/gps_coordinate_transformation.m
@@ -3,10 +3,6 @@ coordinate = input('Ondalıklı bir sayı girin: ');
 [d, m, s] = float2dms(coordinate);
 fprintf('%.6f° = %i° %i" %.5f''\n', coordinate, d, m, s);
 
-coordinate = input('Ondalıklı bir sayı daha girin: ');
-[d, m, s] = float2dms(coordinate);
-fprintf('%.6f° = %i° %i" %.5f''\n', coordinate, d, m, s);
-
 disp('Bu sefer derece, dakika, saniye formatında üç sayı girin.');
 d = input('Derece (tam sayı): ');
 m = input('Dakika (tam sayı): ');
