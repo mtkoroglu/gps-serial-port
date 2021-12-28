6dae0241abb0c43b777772cd1e4fae03a383ef09
mtkoroglu <m.tahakoroglu@gmail.com>
2021-12-28 20:31:48 +0300

    Delete code/read-lat-long directory

--- code/read-lat-long/dms2float.m
+++
@@ -1,3 +0,0 @@
-function coordinateFloat = dms2float(deg, min, sec)
-coordinateFloat = sign(deg) * (abs(deg) + min/60 + sec/3600);
-end
--- code/read-lat-long/float2dms.m
+++
@@ -1,8 +0,0 @@
-function [deg, min, sec] = float2dms(coordinateFloat)
-absCoordinateFloat = abs(coordinateFloat);
-absDeg =  floor(absCoordinateFloat);
-minFloat = 60*(absCoordinateFloat - absDeg);
-deg = sign(coordinateFloat) * absDeg;
-min = floor(minFloat);
-sec = 60*(minFloat - min);
-end
